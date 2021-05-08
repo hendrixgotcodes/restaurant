63e2e5df15c1b5e3077d1d6d6750881086d7b262
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-05-08 09:01:23 +0000

    Initialized App || Tested library implementation

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import ReactFullPage from "@fullpage/react-fullpage"
+
+
+const Fullpage = ()=>(
+
+  <ReactFullPage
+    licenseKey={'14CDBAB4-76C74D16-957F83C7-94169273'}
+    scrollingSpeed= {2000}
+    navigation
+    controlArrows={false}
+    verticalCentered={false}
+    fixedElements = '#header'
+
+    render={({state, fullpageApi})=>(
+
+      <ReactFullPage.Wrapper>
+        <App/>
+      </ReactFullPage.Wrapper>
+
+    )}
+  
+  />
+
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Fullpage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
